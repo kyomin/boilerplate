cecb5bf1123fce3123ee47d5a2ad7b870e0e81e2
kyomin <kim031504@naver.com>
2020-05-31 11:19:43 +0900

    body-parser 미들웨어 추가 및 gitignore 설정 완료

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');   // express 모듈이 export 하는 것은 서버 제작 기능이 담긴 객체를 반환하는 함수일 것이다.
 const app = express();                // 즉, 함수 호출을 통해서 app에 객체를 받는다.
 const port = 5000;
+const bodyParser = require('body-parser');
 
+const { BoilerplateUser } = require('./models/BoilerplateUser');
+
+/* 몽고DB 연결 */
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb+srv://kyomin:0603@youtubeclone-fwhcj.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(mongoURI, {
@@ -14,10 +18,29 @@ mongoose.connect(mongoURI, {
 .catch(err => console.log(err));
 
 
-app.get('/', function (req, res) {
+/* 미들웨어에 bodyParser 추가 */
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.post('/register', (req, res) => {
+    // 회원 가입 시에 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다.
+    const user = new BoilerplateUser(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err });
+        
+        return res.status(200).json({
+            success: true
+        });
+    });
+})
   
-app.listen(port, function () {
-    console.log(`Example app listening on port ${port}!`);
+app.listen(port, () => {
+    console.log(`app listening on port ${port}!`);
 });
